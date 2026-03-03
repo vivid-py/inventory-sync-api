386e531d48abca8f14b35206093a15770e138542
vivid-py <dobrostanandrij109+work@gmail.com>
2026-03-04 00:42:46 +0200

    Upgraded basic code, started to create DataBase

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def root():
     return{
         "message": "Hello My Dear Customer"
     }
+
+@app.get("/products/categories/food")
+def get_products():
+    return {
+        "product1": {
+            "name": "sandwich",
+            "price" : 10,
+            "product_left" : 10
+        }
+        
+    }
